77eab89df89195e2ffdd01347b7395959c35535a
RexShack <rexshackuk@gmail.com>
2022-01-22 18:53:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# RexshackGaming
+- discord : https://discord.gg/s5uSk56B65
+- youtube : https://www.youtube.com/channel/UCikEgGfXO-HCPxV5rYHEVbA
+- github : https://github.com/RexShack
+- website : https://www.rexshack.com
+
+# Built using RedM Game Build 1436 and Framework QBCore RedM Edition
+- https://github.com/qbcore-redm-framework
+- https://docs.fivem.net/docs/server-manual/server-commands/#sv_enforcegamebuild-build
+- add to your server.cfg : sv_enforceGameBuild 1436
+
+# Description
+- allows you to drink from a water-pump for free
+
+# Installation
+1. extract the contents of the rsg_waterpump.zip
+2. add it to your resources folder
+3. Add the following to your "server.cfg"
+
+# Add to server.cfg to start
+ensure rsg_waterpump
+
+# Please consider donating:
+- https://www.buymeacoffee.com/rexshack
---
+++ client/client.lua
@@ -0,0 +1,73 @@
+local QBCore = exports['qbr-core']:GetCoreObject()
+
+-- pump prompt
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local pos, awayFromObject = GetEntityCoords(PlayerPedId()), true
+		local pumpObject = GetClosestObjectOfType(pos, 5.0, GetHashKey("p_waterpump01x"), false, false, false)
+		if pumpObject ~= 0 then
+			local objectPos = GetEntityCoords(pumpObject)
+			if #(pos - objectPos) < 3.0 then
+				awayFromObject = false
+				DrawText3Ds(objectPos.x, objectPos.y, objectPos.z + 1.0, "~g~J~w~ - USE")
+				if IsControlJustReleased(0, 0xF3830D8E) then -- [J]
+					TriggerEvent('drp_waterpump:client:drinking')
+				end
+			end
+		end
+		if awayFromObject then
+			Citizen.Wait(1000)
+		end
+	end
+end)
+
+-- well pump prompt
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local pos, awayFromObject = GetEntityCoords(PlayerPedId()), true
+		local wellpumpObject = GetClosestObjectOfType(pos, 5.0, GetHashKey("p_wellpumpnbx01x"), false, false, false)
+		if wellpumpObject ~= 0 then
+			local objectPos = GetEntityCoords(wellpumpObject)
+			if #(pos - objectPos) < 3.0 then
+				awayFromObject = false
+				DrawText3Ds(objectPos.x, objectPos.y, objectPos.z + 1.0, "~g~J~w~ - USE")
+				if IsControlJustReleased(0, 0xF3830D8E) then -- [J]
+					TriggerEvent('drp_waterpump:client:drinking')
+				end
+			end
+		end
+		if awayFromObject then
+			Citizen.Wait(1000)
+		end
+	end
+end)
+
+-- thirst adjust
+RegisterNetEvent('drp_waterpump:client:drinking')
+AddEventHandler('drp_waterpump:client:drinking', function()
+	QBCore.Functions.Progressbar("drink-pump", "Drinking..", 5000, false, true, {
+		disableMovement = false,
+		disableCarMovement = false,
+		disableMouse = false,
+		disableCombat = true,
+	}, {}, {}, {}, function() -- Done
+		ClearPedTasksImmediately(PlayerPedId())
+		exports['rsg_notify']:DisplayNotification('ahhh fresh', 5000)
+		TriggerServerEvent("QBCore:Server:SetMetaData", "thirst", QBCore.Functions.GetPlayerData().metadata["thirst"] + 100)
+	end, function()
+		exports['rsg_notify']:DisplayNotification('Cancelled', 3000)
+	end)
+end)
+
+function DrawText3Ds(x, y, z, text)
+    local onScreen,_x,_y=GetScreenCoordFromWorldCoord(x, y, z)
+
+    SetTextScale(0.35, 0.35)
+    SetTextFontForCurrentCommand(1)
+    SetTextColor(255, 255, 255, 215)
+    local str = CreateVarString(10, "LITERAL_STRING", text, Citizen.ResultAsLong())
+    SetTextCentre(1)
+    DisplayText(str,_x,_y)
+end
